0ad79233f6cf2854c2aa0324b4de795e21d0a1c3
ooy233 <987876291@qq.com>
2022-05-14 19:20:30 +0800

    testPush

--- Video_Share_Django/UserCommunication/models.py
+++ Video_Share_Django/UserCommunication/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
 # Create your models here.
+import Weblogin.models
 
 
 class VideoPartition(models.Model):
     VideoPartName = models.CharField(max_length=30, null=False)
     VideoPartID = models.AutoField(primary_key=True, null=False)
+
+
+class UserLetter(models.Model):
+    letterUserID = models.OneToOneField('Userinfo', on_delete=models.CASCADE)
+    letterUserName = models.CharField(max_length=30, null=False)
+    letteredUserID = models.OneToOneField('Userinfo', on_delete=models.CASCADE)
+    letteredUserName = models.CharField(max_length=30, null=False)
+    letterText = models.TextField(null=False)
+    letterTime = models.DateTimeField(null=False)
+    letterID = models.AutoField(primary_key=True, null=False)
