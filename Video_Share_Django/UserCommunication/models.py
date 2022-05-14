1a71418e566a4793141b83d53c5b1016ec3666f6
ooy233 <987876291@qq.com>
2022-05-14 18:53:46 +0800

    testPush

--- Video_Share_Django/UserCommunication/models.py
+++ Video_Share_Django/UserCommunication/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 
 
+class VideoPartition(models.Model):
+    VideoPartName = models.CharField(max_length=30, null=False)
+    VideoPartID = models.AutoField(primary_key=True, null=False)
