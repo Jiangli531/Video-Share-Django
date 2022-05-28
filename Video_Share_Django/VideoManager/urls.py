bf9fc6d1eb051422dd55806df819d7fd4dc76c47
ooy233 <987876291@qq.com>
2022-05-28 16:05:57 +0800

    连接到SDK的初始化文件

--- Video_Share_Django/VideoManager/urls.py
+++ Video_Share_Django/VideoManager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
-
+    path('uploadvideo', uploadvideo),
+    path('deletevideo', deletevideo),
+    path('auditvideo', auditvideo),
 ]
--- Video_Share_Django/VideoManager/views.py
+++ Video_Share_Django/VideoManager/views.py
@@ -1,8 +1,15 @@
 import re
 
+# -*- coding=utf-8
+from qcloud_cos import CosConfig
+from qcloud_cos import CosS3Client
+import sys
+import logging
 from django.http import JsonResponse
 from django.shortcuts import render
 
 
 
 # Create your views here.
+
+
