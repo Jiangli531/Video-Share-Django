f53f937d8cee47aa0c8cd117eaa24be404c116f9
ooy233 <987876291@qq.com>
2022-05-28 11:23:27 +0800

    连接到SDK的初始化文件

--- Video_Share_Django/pythonSDK.py
+++ Video_Share_Django/pythonSDK.py
@@ -11,8 +11,8 @@ import logging
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 # 1. 设置用户属性, 包括 secret_id, secret_key, region等。Appid 已在CosConfig中移除，请在参数 Bucket 中带上 Appid。Bucket 由 BucketName-Appid 组成
-secret_id = 'AKIDEVn1ADAKWp9VpzvqiWG8xlQvIZmmZ8UW'     # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-secret_key = 'tsjPvHSSH1fHrPtQxRr6GMclxANURFwV'   # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+secret_id = 'SecretId'     # 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+secret_key = 'SecretKey'   # 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
 region = 'ap-beijing'      # 替换为用户的 region，已创建桶归属的region可以在控制台查看，https://console.cloud.tencent.com/cos5/bucket
                            # COS支持的所有region列表参见https://cloud.tencent.com/document/product/436/6224
 token = None               # 如果使用永久密钥不需要填入token，如果使用临时密钥需要填入，临时密钥生成和使用指引参见https://cloud.tencent.com/document/product/436/14048
