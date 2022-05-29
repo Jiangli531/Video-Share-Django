451f55449442e6f98cb17129e0c9bbc2cdd5b12d
ooy233 <987876291@qq.com>
2022-05-29 18:19:38 +0800

    完成VideoManager中审核部分

--- Video_Share_Django/VideoManager/views.py
+++ Video_Share_Django/VideoManager/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render
 from utils import response_code
 from VideoManager.models import VideoInfo
 from Weblogin.models import UserInfo
-
+from VideoManager.models import AuditRecord
 
 # Create your views here.
 from utils.response_code import SUCCESS
@@ -75,5 +75,35 @@ def deletevideo(request):
         return JsonResponse({'error': 2001, 'msg': '请求方式错误'})
 
 
-#@csrf_exempt  # 跨域设置
-#def auditvideo(request):
+@csrf_exempt  # 跨域设置
+def auditvideo(request):
+    if request.methods == 'POST':
+        postform = request.POST
+        if postform.is_valid():
+            auditid = request.POST.get('AuditID')
+            if AuditRecord.objects.filter(auditID=auditid).exists():
+                audit = AuditRecord.objects.get(auditID=auditid)
+                if request.session.get('is_login', None):
+                    adminid = request.POST.get('AdministratorID')
+                    administrator = UserInfo.objects.get(userID=adminid)
+                    if administrator.userLimit == 1:
+                        result = request.POST.get('AuditResult')
+                        audit.auditTime = request.POST.get('AuditTime')
+                        audit.auditResult = result
+                        audit.auditUser = administrator
+                        audit.save()
+                        if result == 1:
+                            audit.auditVideo.videoUpState = False
+                            audit.auditVideo.save()
+                        return JsonResponse({'error': SUCCESS, 'msg': '审核成功'})
+                    else:
+                        return JsonResponse({'error': 4003, 'msg': '用户无管理员权限'})
+                else:
+                    return JsonResponse({'error': 4002, 'msg': '用户未登录'})
+            else:
+                return JsonResponse({'error': 4001, 'msg': '视频不存在'})
+        else:
+            return JsonResponse({'error': 3001, 'msg': '表单错误'})
+    else:
+        return JsonResponse({'error': 2001, 'msg': '请求方式错误'})
+
