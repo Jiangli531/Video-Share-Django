f6d046c8ecbf5a1c1aa60768be46f513caa383cc
ooy233 <987876291@qq.com>
2022-05-29 17:14:20 +0800

    更改各个模块的URL形式

--- Video_Share_Django/VideoManager/urls.py
+++ Video_Share_Django/VideoManager/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('uploadvideo', uploadvideo),
-    #path('deletevideo', deletevideo),
+    path('uploadvideo/', uploadvideo),
+    path('deletevideo/', deletevideo),
     #path('auditvideo', auditvideo),
 ]
